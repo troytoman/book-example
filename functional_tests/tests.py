35acd5b90d89d83d76659f37f0d9e81154c5d21f
Troy Toman <troy@tomanator.com>
2014-04-26 12:04:26 -0500

    new list URL, view and template to display lists only

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -67,7 +67,7 @@ class NewVistorTest(LiveServerTestCase):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith ...
-        inputbox = self.browser.current_url
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
